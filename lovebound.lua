0a3c766de966a6827faf43a03b98d0b8865e978b
William Ferreira <wqferr@gmail.com>
2020-12-03 19:51:48 -0300

    Convert tabs to spaces

--- lovebound.lua
+++ lovebound.lua
@@ -5,59 +5,53 @@ keys.pressedKeys = {}
 keys.context = "default"
 
 function keys:newSystem()
-	return setmetatable({}, keys)
+    return setmetatable({}, keys)
 end
 
 function keys:newKeybind(info)
-	assert(type(info) == "table", [[
-		Passed payload needs to be a table. i.e.
-		{
-			keys = {},
-			exclusive = false,
-			ordered = false,
-			context = "default"
-		}
-	]])
-	local default = {
-		keys = {},
-		exclusive = false,
-		ordered = false,
-		context = "default"
-	}
+    assert(
+        type(info) == "table",
+        [[
+        Passed payload needs to be a table. i.e.
+        {
+            keys = {},
+            exclusive = false,
+            ordered = false,
+            context = "default"
+        }
+    ]]
+    )
+    local default = {
+        keys = {},
+        exclusive = false,
+        ordered = false,
+        context = "default"
+    }
 
-	for k,v in pairs(info) do
-		default[k] = v
-	end
+    for k, v in pairs(info) do
+        default[k] = v
+    end
 
-	
-
-	-- require exclusive key hit aka no other buttons can be pushed.
-	-- press/release/both
-	-- keyCombination, order specific
-	-- context
-
+    -- require exclusive key hit aka no other buttons can be pushed.
+    -- press/release/both
+    -- keyCombination, order specific
+    -- context
 end
 
 -- context is what state
 function keys:addContext(context)
-
 end
 function keys:removeContext(context)
-
 end
 
-
-
-
-
 function keys.keypressed(key)
-	keys.pressedKeys[key] = true
+    keys.pressedKeys[key] = true
 end
 
 function keys.keyreleased(key)
-	keys.pressedKeys[key] = false
+    keys.pressedKeys[key] = false
 end
 
 function keys.setContext(context)
-	keys.context = context
+    keys.context = context
 end
